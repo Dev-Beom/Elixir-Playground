b7a85ab48e4e7ed1838c038ca053804613725450
Benn <66074802+Dev-Beom@users.noreply.github.com>
2022-02-15 20:11:29 +0900

    refactor, short hand capture notation

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,34 +1,16 @@
 defmodule Secrets do
-  def secret_add(secret) do
-    fn num ->
-      num + secret
-    end
-  end
+  def secret_add(secret), do: &(&1 + secret)
 
-  def secret_subtract(secret) do
-    fn num -> num - secret end
-  end
+  def secret_subtract(secret), do: &(&1 - secret)
 
-  def secret_multiply(secret) do
-    fn num -> num * secret end
-  end
+  def secret_multiply(secret), do: &(&1 * secret)
 
-  def secret_divide(secret) do
-    fn num -> div(num, secret) end
-  end
+  def secret_divide(secret), do: &div(&1, secret)
 
-  def secret_and(secret) do
-    fn num -> Bitwise.&&&(num, secret) end
-  end
+  def secret_and(secret), do: &Bitwise.&&&(&1, secret)
 
-  def secret_xor(secret) do
-    fn num -> Bitwise.^^^(num, secret) end
-  end
+  def secret_xor(secret), do: &Bitwise.^^^(&1, secret)
 
-  def secret_combine(secret_function1, secret_function2) do
-    fn num ->
-      secret_function1.(num)
-      |> secret_function2.()
-    end
-  end
+  def secret_combine(secret_function1, secret_function2),
+    do: &(secret_function1.(&1) |> secret_function2.())
 end
