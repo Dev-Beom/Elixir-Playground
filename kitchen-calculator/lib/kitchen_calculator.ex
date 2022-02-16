63d963d883869c1bbce358e96b6d950f855b596a
Benn <66074802+Dev-Beom@users.noreply.github.com>
2022-02-17 00:39:20 +0900

    refactor, simple code

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -7,37 +7,15 @@ defmodule KitchenCalculator do
     tablespoon: 15
   }
 
-  def get_volume(volume_pair) do
-    {_, value} = volume_pair
-    value
-  end
+  def get_volume({_, volume}), do: volume
 
-  def to_milliliter(volume_pair) do
-    {type, value} = volume_pair
+  def to_milliliter({unit, volume}), do: {:milliliter, volume * @ratios[unit]}
 
-    case type do
-      :cup -> {:milliliter, value * 240}
-      :fluid_ounce -> {:milliliter, value * 30}
-      :teaspoon -> {:milliliter, value * 5}
-      :tablespoon -> {:milliliter, value * 15}
-      _ -> volume_pair
-    end
-  end
-
-  def from_milliliter(volume_pair, unit) do
-    value = get_volume(volume_pair)
-
-    case unit do
-      :cup -> {unit, value / 240}
-      :fluid_ounce -> {unit, value / 30}
-      :teaspoon -> {unit, value / 5}
-      :tablespoon -> {unit, value / 15}
-      _ -> {unit, value}
-    end
-  end
+  def from_milliliter({:milliliter, volume}, unit), do: {unit, volume / @ratios[unit]}
 
   def convert(volume_pair, unit) do
-    to_milliliter(volume_pair)
+    volume_pair
+    |> to_milliliter()
     |> from_milliliter(unit)
   end
 end
