e0f9ddcd8346e6d6ba2e7d962cc0028fd6cb6b06
benn-dev <66074802+Dev-Beom@users.noreply.github.com>
2022-04-07 18:25:55 +0900

    backup, 20220407

--- advent_of_code_2019/10_Monitoring_Station.ex
+++ advent_of_code_2019/10_Monitoring_Station.ex
@@ -28,9 +28,45 @@ defmodule Day10 do
     base = part_1(str)
     targets = list |> list_to_ast_list()
 
-    get_radians(base, targets)
-    |> Enum.sort_by(fn e -> e.radian end, :desc)
-    |> IO.inspect()
+    result =
+      get_radians(base, targets)
+      |> Enum.sort_by(& &1.radian, :desc)
+      |> find_deleted_ast_by_idx(base, 200, 1)
+      |> IO.inspect()
+
+    IO.inspect(result.x * 100 + result.y)
+  end
+
+  def find_deleted_ast_by_idx([head | tails] = list, base, find_idx, curr_idx) do
+    # 현재와 다음의 라디언이 같은경우
+    # 베이스보다 가장 가가운 라디언을 찾아서 제거
+    if find_idx === curr_idx do
+      head
+    end
+
+    if head.radian === hd(tails).radian do
+      rebased_list = rebase_minimun_distance_ast(tails, base, head)
+      find_deleted_ast_by_idx(rebased_list, base, find_idx, curr_idx)
+    else
+      if find_idx === curr_idx do
+        head
+      else
+        find_deleted_ast_by_idx(tails, base, find_idx, curr_idx + 1)
+      end
+    end
+  end
+
+  # 같은 각도에 위치한 행성 중 가장 가까운 행성을 제외하고 리스트의 맨 뒤로 보냅니다.
+  def rebase_minimun_distance_ast([head | tails] = list, base, curr_min_ast) do
+    if head.radian != hd(tails).radian do
+      list
+    else
+      if get_distance(base, curr_min_ast) > get_distance(base, head) do
+        rebase_minimun_distance_ast(tails ++ [curr_min_ast], base, head)
+      else
+        rebase_minimun_distance_ast(tails ++ [head], base, curr_min_ast)
+      end
+    end
   end
 
   def get(list, x, y), do: Enum.at(list, y) |> Enum.at(x)
@@ -44,14 +80,14 @@ defmodule Day10 do
 
   def get_radian(base, target), do: :math.atan2(target.x - base.x, target.y - base.y)
 
-  def get_degree(base, target) do
-    get_radian(base, target) * 180 / :math.pi()
-  end
+  def get_distance(base, target),
+    do: :math.sqrt(:math.pow(abs(target.x - base.x), 2) + :math.pow(abs(target.y - base.y), 2))
 
-  def get_radians(base, targets) do
-    targets
-    |> Enum.map(&Map.put(&1, :radian, get_radian(base, &1)))
-  end
+  # base.x = A, base.y = B, target.x = C, target.y = D
+  # Math.sqrt( Math.pow( Math.abs(C - A), 2 ) + Math.pow( Math.abs(D - B), 2 ) )
+
+  def get_radians(base, targets),
+    do: targets |> Enum.map(&Map.put(&1, :radian, get_radian(base, &1)))
 
   def get_monitoring_cnt(_, [], list),
     do: %{count: length(list), x: Enum.at(list, 0).x, y: Enum.at(list, 0).y}
@@ -73,12 +109,9 @@ defmodule Day10 do
   def add_custom_set(list, element) when length(list) == 0, do: [element | list]
 
   def add_custom_set(list, element) when length(list) > 0 do
-    cnt = Enum.count(list, &(&1.radian == element.radian))
-
-    if cnt > 0 do
-      list
-    else
-      [element | list]
+    cond do
+      Enum.count(list, &(&1.radian == element.radian)) > 0 -> list
+      true -> [element | list]
     end
   end
 end
