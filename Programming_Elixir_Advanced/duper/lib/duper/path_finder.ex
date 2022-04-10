efd106155f2e183bcab544e7da1573f2504bda8c
Dev-Beom <youngbeom0908@gmail.com>
2022-04-11 01:19:03 +0900

    refactor, 오타 수정

--- Programming_Elixir_Advanced/duper/lib/duper/path_finder.ex
+++ Programming_Elixir_Advanced/duper/lib/duper/path_finder.ex
@@ -21,7 +21,7 @@ defmodule Duper.PathFinder do
   @doc """
   파일 시스탬 내 모든 파일의 경로를 하나씩 반환하는 함수
   """
-  def hanlde_call(:next_path, _from, dir_walker) do
+  def handle_call(:next_path, _from, dir_walker) do
     path =
       case DirWalker.next(dir_walker) do
         [path] -> path
